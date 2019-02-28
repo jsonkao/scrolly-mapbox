0e5c68a1eaf1a631cfcf369e5e1bfcf8ff3986b4
Jason Kao <jason.kao@columbia.edu>
2019-02-27 22:58:19 -0500

    src!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import ReactMapGL from 'react-map-gl';
 
 class App extends Component {
+  state = {
+    viewport: {
+      width: 400,
+      height: 400,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8,
+    },
+  };
+
   render() {
     return (
-      <div>App</div>
+      <ReactMapGL
+        {...this.state.viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        onViewportChange={viewport => this.setState({ viewport })}
+      />
     );
   }
 }

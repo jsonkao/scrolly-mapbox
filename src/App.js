f5947ef04f06ac2205757bae3cb48621921962b9
Jason Kao <jason.kao@columbia.edu>
2019-03-01 14:58:27 -0500

    implemented flyto (NY and SF)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
 import React, { Component } from 'react';
-import ReactMapGL from 'react-map-gl';
+import { Scrollama, Step } from 'react-scrollama';
+import ReactMapGL, { FlyToInterpolator } from 'react-map-gl';
+import { easeCubic } from 'd3-ease';
+import injectSheet from 'react-jss';
+
+const styles = {
+  graphicContainer: {
+    position: 'sticky',
+  },
+  sticky: {
+    margin: 0,
+  },
+};
+
+const steps = ['Yote', 'Yotatl'];
 
 class App extends Component {
   state = {
     viewport: {
-      width: 400,
-      height: 400,
+      width: window.innerWidth,
+      height: window.innerHeight,
       latitude: 37.7577,
       longitude: -122.4376,
       zoom: 8,
     },
   };
 
+  onViewportChange = viewport => {
+    this.setState({ viewport });
+  };
+
+  goToSF = () => {
+    const viewport = {
+      ...this.state.viewport,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8,
+      transitionEasing: easeCubic,
+    };
+    this.setState({ viewport });
+  };
+
+  goToNYC = () => {
+    const viewport = {
+      ...this.state.viewport,
+      longitude: -74.1,
+      latitude: 40.7,
+      zoom: 14,
+      transitionEasing: easeCubic,
+    };
+    this.setState({ viewport });
+  };
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <ReactMapGL
-        {...this.state.viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-        onViewportChange={viewport => this.setState({ viewport })}
-      />
+      <div className={classes.graphicContainer}>
+        <button onClick={this.goToNYC}>New York City</button>
+        <button onClick={this.goToSF}>SF</button>
+        <figure className={classes.sticky}>
+          <ReactMapGL
+            {...this.state.viewport}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+            transitionDuration={5000}
+            transitionInterpolator={new FlyToInterpolator()}
+            onViewportChange={this.onViewportChange}
+          />
+        </figure>
+        <Scrollama
+          offset={0.5}
+          onStepEnter={this.onStepEnter}
+          onStepExit={this.onStepExit}
+        >
+          {steps.map(text => (
+            <Step key={text}>
+              <div className={classes.step}>
+                <p
+                  className={classes.stepText}
+                  dangerouslySetInnerHTML={{ __html: text }}
+                />
+              </div>
+            </Step>
+          ))}
+        </Scrollama>
+      </div>
     );
   }
 }
 
-export default App;
+export default injectSheet(styles)(App);
